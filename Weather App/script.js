10f5dc1f7070ae3206b93012e2f3557a26beb4bd
Ameet9 <ameetnayak919@gmail.com>
2024-10-10 11:44:30 +0530

    Weather app updated

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,5 +1,9 @@
 const searchInput = document.querySelector(".searchInput");
+const locationButton = document.querySelector(".locationButton");
 const currentWeatherDiv = document.querySelector(".currentWeather");
+const hourlyWeatherDiv = document.querySelector(".hourlyWeather .weatherList");
+const weatherSectionDiv = document.querySelector(".weatherSection");
+const noResultsDiv = document.querySelector(".noResults");
 
 const API_KEY = "f49300747ffb4ac1b53100814240910";
 
@@ -10,11 +14,10 @@ const displayHourlyForecast = (hourData) => {
     const forecastTime = new Date(time).getTime();
     return forecastTime >= currentHour && forecastTime <= next24Hours;
   });
-  console.log(next24HoursData);
-  const hourlyWeatherHTML = next24HoursData
+  hourlyWeatherDiv.innerHTML = next24HoursData
     .map((i) => {
       const temperature = Math.floor(i.temp_c);
-      const time = i.time;
+      const time = i.time.split(" ")[1].substring(0, 5);
       const imageUrl = i.condition.icon;
 
       return `<li class="weatherItem">
@@ -26,8 +29,11 @@ const displayHourlyForecast = (hourData) => {
     .join("");
 };
 
-const getWeatherDetails = async (cityName) => {
-  const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
+const getWeatherDetails = async (API_URL) => {
+  window.innerWidth <= 768 && searchInput.blur();
+  noResultsDiv.style.visibility = "hidden";
+  weatherSectionDiv.style.visibility = "visible";
+  // document.body.classList.remove("showNoResults");
   try {
     const resp = await fetch(API_URL);
     const data = await resp.json();
@@ -44,13 +50,37 @@ const getWeatherDetails = async (cityName) => {
       ...data.forecast.forecastday[1].hour,
     ];
     displayHourlyForecast(combinedHourlyData);
+    searchInput.value = data.location.name;
   } catch (error) {
+    // document.body.classList.add("showNoResults");
+    noResultsDiv.style.visibility = "visible";
+    weatherSectionDiv.style.visibility = "hidden";
     console.log(error);
   }
 };
+const setupWeatherRequest = (cityName) => {
+  const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
+  getWeatherDetails(API_URL);
+};
+
 searchInput.addEventListener("keyup", (e) => {
   const cityName = searchInput.value.trim();
   if (e.key === "Enter" && cityName) {
-    getWeatherDetails(cityName);
+    setupWeatherRequest(cityName);
   }
 });
+
+locationButton.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
+      getWeatherDetails(API_URL);
+    },
+    (error) => {
+      console.log(error.message);
+    }
+  );
+});
+
+setupWeatherRequest("New Delhi");
